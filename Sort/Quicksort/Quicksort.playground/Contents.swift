5a33ab9e45fc6714632af87a6877c9c3bb9e53c1
李泽群 <lizequn@kefeizhu.com>
2019-06-20 10:00:58 +0800

    添加注释

--- Sort/Quicksort/Quicksort.playground/Contents.swift
+++ Sort/Quicksort/Quicksort.playground/Contents.swift
@@ -1,17 +1,24 @@
 // 快排low版
 func Quicksort(arr: [Int]) -> [Int] {
+    // 结束条件
     if arr.count <= 1 {
         return arr
     }
     
+    // 基准值下标
     let midIndex = arr.count/2
+    // 基准值
     let mid = arr[midIndex]
+    
+    //
     var lowArr = [Int]()
     var heightArr = [Int]()
     
+    // 待排序数组
     var beSortedArr = arr
     beSortedArr.remove(at: midIndex)
     
+    // 如果数组里的数比基准值小，就放lowArr，否则放heightArr
     for c in beSortedArr {
         if c < mid {
             lowArr.append(c)
@@ -20,6 +27,7 @@ func Quicksort(arr: [Int]) -> [Int] {
         }
     }
     
+    // 规模不断减小，分治法
     return Quicksort(arr: lowArr) + [mid] + Quicksort(arr: heightArr)
 }
 
